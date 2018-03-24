eacc50638b04563e24f4e5e09cdb93272821934a
Yoshihiro Tanaka <contact@cordea.jp>
2018-03-24 18:24:03 +0900

    provide what api client needs

--- app/src/main/java/jp/cordea/vultrshowcase/AppModule.kt
+++ app/src/main/java/jp/cordea/vultrshowcase/AppModule.kt
@@ -1,6 +1,24 @@
 package jp.cordea.vultrshowcase
 
+import android.app.Application
+import android.content.Context
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module(includes = [ViewModelModule::class])
-class AppModule
+class AppModule {
+
+    @Provides
+    fun provideContext(application: Application): Context =
+            application.applicationContext
+
+    @Provides
+    fun provideRetrofitBuilder(): Retrofit.Builder =
+            Retrofit.Builder()
+                    .baseUrl(BuildConfig.API_BASE_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+}
