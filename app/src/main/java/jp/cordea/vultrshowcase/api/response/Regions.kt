c2e8a8733eabf9b43ae53b2111cad562023e53f2
Yoshihiro Tanaka <contact@cordea.jp>
2018-03-24 18:24:12 +0900

    add response models

---
+++ app/src/main/java/jp/cordea/vultrshowcase/api/response/Regions.kt
@@ -0,0 +1,18 @@
+package jp.cordea.vultrshowcase.api.response
+
+import com.squareup.moshi.Json
+
+data class Regions(
+        val regions: Map<String, Region>
+)
+
+data class Region(
+        @Json(name = "DCID") val id: Int,
+        val name: String,
+        val country: String,
+        val continent: String,
+        val state: String,
+        @Json(name = "ddos_protection") val ddosProtection: Boolean,
+        @Json(name = "block_storage") val blockStorage: Boolean,
+        @Json(name = "regioncode") val regionCode: String
+)
